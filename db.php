90a00c04d2b03ec28c5e9c1edec4bc263bdc7f30
RajanMohammad <rajanmohammad@hotmail.com>
2025-10-27 13:22:46 +0100

    Update db.php

--- db.php
+++ db.php
@@ -8,3 +8,5 @@ $password = 'abf8ramoh0645';
 $database = 'ramoh0645';
 
 $db = mysqli_connect($host, $username, $password, $database) or die ("Kan ikke koble til database-serveren");
+/* tilkobling til database-serveren utført */
+?>
