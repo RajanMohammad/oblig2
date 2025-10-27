1f820cbd98c2a68f9d653b3c10d143769f758f83
RajanMohammad <rajanmohammad@hotmail.com>
2025-10-27 13:29:43 +0100

    Update registrer_student.php

--- registrer_student.php
+++ registrer_student.php
@@ -1,17 +1,41 @@
 <meta charset ="UTF-8">
+<?php /* oblig2/registrer_student.php */
+/* 
+/*Programmet lager et html-skjema for registrering av ny student i databasen
+/* Programmet registrerer studenten når skjema er sendt inn
+*/
+?>
+
+<h2>Registrer ny student</h2>
+<form method="post" action="registrer_student.php">  
+    <label for="brukernavn">Brukernavn:</label>
+    <input type="text" id="brukernavn" name="brukernavn" required><br><br>
+    
+    <label for="fornavn">Fornavn:</label>
+    <input type="text" id="fornavn" name="fornavn" required><br><br>
+    
+    <label for="etternavn">Etternavn:</label>
+    <input type="text" id="etternavn" name="etternavn" required><br><br>
+    
+    <label for="klassekode">Klassekode:</label>
+    <input type="text" id="klassekode" name="klassekode" required><br><br>
+    
+    <input type="submit" value="Registrer student">
+</form>
 <?php
-include ('db.php');
-if (isset($_POST["klassekode"])) { // sjekker om skjema er sendt inn
-
-$brukernavn = mysqli_real_escape_string($db, $_POST["brukernavn"]);
-$fornavn = mysqli_real_escape_string($db, $_POST["fornavn"]);   
-$etternavn = mysqli_real_escape_string($db, $_POST["etternavn"]);
-$klassekode = mysqli_real_escape_string($db, $_POST["klassekode"]);
-
-$sqlsetning = "INSERT INTO student (brukernavn, fornavn, etternavn, klassekode) VALUES ('$brukernavn', '$fornavn', '$etternavn', '$klassekode');";
-mysqli_query($db, $sqlsetning) or die ("Ikke mulig å registrere data i databasen");
-
-print ("<h2>Følgende student er nå registrert:</h2>");
-print ("Brukernavn: $brukernavn <br> Fornavn: $fornavn <br> Etternavn: $etternavn <br> Klassekode: $klassekode <br>");
+if (isset($_POST["brukernavn"]))
+{ 
+    include ('db.php');
+    
+    $brukernavn = mysqli_real_escape_string($db, $_POST["brukernavn"]);
+    $fornavn = mysqli_real_escape_string($db, $_POST["fornavn"]);
+    $etternavn = mysqli_real_escape_string($db, $_POST["etternavn"]);
+    $klassekode = mysqli_real_escape_string($db, $_POST["klassekode"]);
+    
+    $sqlsetning = "INSERT INTO student (brukernavn, fornavn, etternavn, klassekode) VALUES ('$brukernavn', '$fornavn', '$etternavn', '$klassekode');";
+    mysqli_query($db, $sqlsetning) or die ("Ikke mulig å registrere data i databasen");
+    
+    print ("<h2>Følgende student er nå registrert:</h2>");
+    print ("Brukernavn: $brukernavn <br> Fornavn: $fornavn <br> Etternavn: $etternavn <br> Klassekode: $klassekode <br>");
 }
 ?>
