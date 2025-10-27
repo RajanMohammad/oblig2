3aa2968cb8044e7a842c31bb82eaaf15442c2c9c
RajanMohammad <rajanmohammad@hotmail.com>
2025-10-27 14:14:11 +0100

    Update slett_klasse.php

--- slett_klasse.php
+++ slett_klasse.php
@@ -1,33 +1,47 @@
     <meta charset ="UTF-8">
+    <?php /* oblig2/slett_klasse.php */
+    /*
+    /*Programmet lager et skjema for sletting av en klasse fra databasen
+    /*Programmet sletter klassen når skjema er sendt inn    
+    */
+
+    <script src="funksjoner.js"></script>
+
+    <h2>Slett en klasse</h2>
+    <form method="post" action="slett_klasse.php" onsubmit="return bekreftSletting();">  
+        <label for="klassekode">Velg klasse å slette:</label>
+        <select name="klassekode" id="klassekode">
+    </form>
+
     <?php
-    include ('db.php');
-    // Hvis skjema er sendt inn, slett klassen
-    if (isset($_POST["klassekode"])) {
-    $klassekode = mysqli_real_escape_string($db, $_POST["klassekode"]);
+    if (isset($_POST["klassekode"]))
+    { 
+    $klassekode=$_POST ["klassekode"];
 
-    // SQL for å slette klassen
-    $sqlsetning = "DELETE FROM klasse WHERE klassekode = '$klassekode';";
-        mysqli_query($db, $sqlsetning) or die ("Ikke mulig å slette data fra databasen");
-        echo "<h2>Følgende klasse er nå slettet:</h2>";
-        echo "Klassekode: $klassekode <br>";
-        echo "<a href='slett_klasse.php'>Slett en annen klasse</a><br>";
-        echo "<a href='index.php'>Tilbake til hovedsiden</a>";
+    if (!$klassekode)
+    
+    {
+    print ("klassekode m&aring; fylles ut");
     }
-    // Hent alle klasser for valg i skjema
-    $sql_hent = "SELECT klassekode, klassenavn FROM klasse ORDER BY klassekode;";
-    $resultat = mysqli_query($db, $sql_hent) or die ("Ikke mulig å hente data fra databasen");
+    else
+    {
+    include ('db.php'); /* tilkobling til database-server utført og valg av database foretatt */
+    $sqlsetning="DELETE FROM klasse WHERE klassekode='$klassekode';";
+    $sqlresultat=mysqli_query($db, $sqlsetning) or die ("Ikke mulig å slette data fra databasen");
+    $antallRader=mysqli_num_rows($sqlresultat);
 
-    // Lag skjema for sletting av klasse
-    echo "<h2>Slett en klasse</h2>";
-    echo "<form method='post' action='slett_klasse.php'>";
-    echo "<label for='klassekode'>Velg klasse å slette:</label>";
-    echo "<select name='klassekode' id='klassekode'>";
-    while ($rad = mysqli_fetch_assoc($resultat)) {
-        $kode = $rad['klassekode'];
-        $navn = $rad['klassenavn'];
-        echo "<option value='$kode'>$kode - $navn</option>";
+    if ($antallRader==0) /* klassen er ikke registert  */
+    {
+    print ("klassen finnes ikke i databasen");
     }
-    echo "</select><br><br>";
-    echo "<input type='submit' value='Slett klasse'>";
-    echo "</form>";
-    ?>
+    else
+    {
+    $sqlsetning="DELETE FROM klasse WHERE klassekode='$klassekode';";
+    mysqli_query($db, $sqlsetning) or die ("Ikke mulig &aring; slette data fra databasen");
+    /* SQL-setning sendt til database-serveren */
+
+    print ("F&oslash;lgende poststed er n&aring; slettet: $klassekode <br>");
+    }
+    }
+    }
+?>   
