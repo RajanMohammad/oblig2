0008402ce99302b52ae78354a4c6cf559aeabe83
RajanMohammad <rajanmohammad@hotmail.com>
2025-10-29 16:17:19 +0100

    Refactor vis_alle_studenter.php with comments
    Updated the script to improve readability and added comments for clarity.

--- vis_alle_studenter.php
+++ vis_alle_studenter.php
@@ -4,23 +4,25 @@
 /*Programmet viser alle studenter i databasen
 */
 
-include ('db.php');
+include ('db.php'); /* tilkobling til database-server utført og valg av database foretatt */
 
 
 $sqlsetning = "SELECT * FROM student ORDER BY etternavn;";
 $sqlresultat = mysqli_query($db, $sqlsetning) or die ("Ikke mulig å hente data fra databasen");
-$sqlantallRader = mysqli_num_rows($sqlresultat);
+/* SQL-setning sendt til database-serveren */
+$sqlantallRader = mysqli_num_rows($sqlresultat); /* antall rader i resultatet */
 
 
 print "<h2>Alle studenter</h2>";
 print "<table border='1'>";
 print "<tr> <th> Studentnr </th> <th> Fornavn </th> <th> Etternavn </th> <th> Klassekode </th> </tr>";
 
-while ($rad = mysqli_fetch_assoc($sqlresultat)) {
-    $studentnr = $rad["studentnr"];
-    $fornavn = $rad["fornavn"];
-    $etternavn = $rad["etternavn"];
-    $klassekode = $rad["klassekode"];
+for ($r = 1; $r <= $sqlantallRader; $r++) 
+    {
+    $studentnr = $rad["studentnr"]; /* eller studentnr = $rad[0]; */
+    $fornavn = $rad["fornavn"]; /* eller fornavn = $rad[1]; */
+    $etternavn = $rad["etternavn"]; /* eller etternavn = $rad[2]; */
+    $klassekode = $rad["klassekode"]; /* eller klassekode = $rad[3]; */
     print "<tr> <td> $studentnr </td> <td> $fornavn </td> <td> $etternavn </td> <td> $klassekode </td> </tr>";
 }
 
