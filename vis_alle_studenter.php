1278fe0f25841ea96843414bb31b20f3ded418e7
RajanMohammad <rajanmohammad@hotmail.com>
2025-10-29 16:08:12 +0100

    Update vis_alle_studenter.php

--- vis_alle_studenter.php
+++ vis_alle_studenter.php
@@ -1,23 +1,28 @@
 <meta charset ="UTF-8">
-<?php
+<?php /* oblig2/vis_alle_studenter.php */
+/*
+/*Programmet viser alle studenter i databasen
+*/
+
 include ('db.php');
 
-// Hent alle studenter
+
 $sqlsetning = "SELECT * FROM student ORDER BY etternavn;";
 $sqlresultat = mysqli_query($db, $sqlsetning) or die ("Ikke mulig å hente data fra databasen");
+$sqlantallRader = mysqli_num_rows($sqlresultat);
 
-// Lag tabell
-echo "<h2>Alle studenter</h2>";
-echo "<table border='1'>";
-echo "<tr> <th> Studentnr </th> <th> Fornavn </th> <th> Etternavn </th> <th> Klassekode </th> </tr>";
+
+print "<h2>Alle studenter</h2>";
+print "<table border='1'>";
+print "<tr> <th> Studentnr </th> <th> Fornavn </th> <th> Etternavn </th> <th> Klassekode </th> </tr>";
 
 while ($rad = mysqli_fetch_assoc($sqlresultat)) {
     $studentnr = $rad["studentnr"];
     $fornavn = $rad["fornavn"];
     $etternavn = $rad["etternavn"];
     $klassekode = $rad["klassekode"];
-    echo "<tr> <td> $studentnr </td> <td> $fornavn </td> <td> $etternavn </td> <td> $klassekode </td> </tr>";
+    print "<tr> <td> $studentnr </td> <td> $fornavn </td> <td> $etternavn </td> <td> $klassekode </td> </tr>";
 }
 
-echo "</table>";
+print "</table>";
 ?>
