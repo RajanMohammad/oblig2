0f4f403cc19b240cd2d4b91e5fac415d6cc69a73
RajanMohammad <rajanmohammad@hotmail.com>
2025-10-29 16:45:27 +0100

    Fix SQL variable naming and comments in PHP file

--- vis_alle_klasser.php
+++ vis_alle_klasser.php
@@ -5,17 +5,17 @@
 */
 include ('db.php'); /* tilkobling til database-server utført og valg av database foretatt */
 
-$sqlsetning = "SELECT * FROM klasse order by klassekode;";
-$sqlresultat = mysqli_query($db, $sqlsetning) or die ("Ikke mulig å hente data fra databasen");
+$sqlSetning = "SELECT * FROM klasse;";
+$sqlResultat = mysqli_query($db, $sqlsetning) or die ("Ikke mulig å hente data fra databasen");
 /* SQL-setning sendt til database-serveren */
-$antallRader = mysqli_num_rows($sqlresultat); /* antall rader i resultatet beregnet */
+$antallRader = mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
 
 print ("<h2>Alle klasser</h2>");
 print ("<table border='1'>");
 print ("<tr> <th> Klassekode </th> <th> Klassenavn </th> <th> Studiumkode </th> </tr>");
 
 for ($r = 1; $r <= $antallRader; $r++) {
-    $rad = mysqli_fetch_array($sqlresultat); /* ny rad hentet fra sparringsresultatet */
+    $rad = mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
     $klassekode = $rad["klassekode"]; /* eller klassekode = $rad[0]; */
     $klassenavn = $rad["klassenavn"]; /* eller klassenavn = $rad[1]; */
     $studiumkode = $rad["studiumkode"]; /* eller studiumkode = $rad[2]; */
