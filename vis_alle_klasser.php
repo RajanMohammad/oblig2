e5942c6a7bcdc62c0b759caf5ef9290c77d4fb93
RajanMohammad <rajanmohammad@hotmail.com>
2025-10-29 16:15:50 +0100

    Update vis_alle_klasser.php

--- vis_alle_klasser.php
+++ vis_alle_klasser.php
@@ -3,20 +3,21 @@
 /*
 /*Programmet viser alle klasser i databasen
 */
-include ('db.php');
+include ('db.php'); /* tilkobling til database-server utført og valg av database foretatt */
 
 $sqlsetning = "SELECT * FROM klasse order by klassekode;";
 $sqlresultat = mysqli_query($db, $sqlsetning) or die ("Ikke mulig å hente data fra databasen");
-$antallRader = mysqli_num_rows($sqlresultat);
+/* SQL-setning sendt til database-serveren */
+$antallRader = mysqli_num_rows($sqlresultat); /* antall rader i resultatet beregnet */
 
 print ("<h2>Alle klasser</h2>");
 print ("<table border='1'>");
 print ("<tr> <th> Klassekode </th> <th> Klassenavn </th> <th> Studiumkode </th> </tr>");
 
 for ($r = 1; $r <= $antallRader; $r++) {
-    $rad = mysqli_fetch_array($sqlresultat);
-    $klassekode = $rad["klassekode"];
-    $klassenavn = $rad["klassenavn"];
+    $rad = mysqli_fetch_array($sqlresultat); /* ny rad hentet fra sparringsresultatet */
+    $klassekode = $rad["klassekode"]; /* eller klassekode = $rad[0]; */
+    $klassenavn = $rad["klassenavn"]; /* eller klassenavn = $rad[1]; */
 $studiumkode = $rad["studiumkode"];
     print ("<tr> <td> $klassekode </td> <td> $klassenavn </td> <td> $studiumkode </td> </tr>");
 }
