fd5b9e815246c0600ef69cf2f081fa3b4801202a
RajanMohammad <rajanmohammad@hotmail.com>
2025-11-03 13:39:56 +0100

    Update database connection to use getenv

--- db.php
+++ db.php
@@ -2,10 +2,10 @@
 
 /* Programmet kobler til database-server og velger database */
 
-$host = 'ramoh0645';
-$username = 'ramoh0645';
-$password = 'abf8ramoh0645';
-$database = 'ramoh0645';
+$host = getenv ('ramoh0645');
+$username = getenv ('ramoh0645');
+$password = getenv ('abf8ramoh0645');
+$database = getenv ('ramoh0645');
 
 $db = mysqli_connect($host, $username, $password, $database) or die ("Kan ikke koble til database-serveren");
 /* tilkobling til database-serveren utført */
