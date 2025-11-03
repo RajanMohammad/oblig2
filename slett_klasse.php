fbca79b07706ce598042e8ea4d8f36576bc68ca2
RajanMohammad <rajanmohammad@hotmail.com>
2025-11-03 21:01:27 +0100

    Update slett_klasse.php

--- slett_klasse.php
+++ slett_klasse.php
@@ -16,27 +16,28 @@
 bekreft()">
 Emne <select name="klassekode" id="klassekode">
 <?php print("<option value=''>velg klasse </option>");
-include("dynamiske-funksjoner.php"); listeboksEmnekode(); ?>
+include("dynamiske-funksjoner.php"); listeboksKlassekode(); ?>
 </select> <br/>
-<input type="submit" value="Slett emne" name="slettEmneKnapp" id="slettEmneKnapp" />
+<input type="submit" value="Slett klasse" name="slettKlasseKnapp" id="slettKlasseKnapp" />
 </form>
 <?php
 if (isset($_POST ["slettKlasseKnapp"]))
 {
 include("db.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
+
 $klassekode=$_POST ["klassekode"];
+
 if (!$klassekode)
 {
-print ("Det er ikke valgt noe emne");
+print ("Det er ikke valgt noe klasse");
 }
 else
 {
-include("db-tilkobling.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
-$sqlSetning="DELETE FROM emne WHERE klassekode='$klassekode';";
+include("db.php"); /* tilkobling til database-serveren utført og valg av database foretatt */
+$sqlSetning="DELETE FROM klasse WHERE klassekode='$klassekode';";
 mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
 /* SQL-setning sendt til database-serveren */
-print ("F&oslash;lgende emne er n&aring; slettet: $klassekode <br />");
+print ("F&oslash;lgende klasse er n&aring; slettet: $klassekode <br />");
 }
 }
 ?>
-
