38831aff2654ec6cb7db5f268d0ccfd37686bf7f
RajanMohammad <rajanmohammad@hotmail.com>
2025-11-03 03:31:39 +0100

    Update vis_alle_klasser.php

--- vis_alle_klasser.php
+++ vis_alle_klasser.php
@@ -1,25 +1,27 @@
-<meta charset="UTF-8">
-<?php /* oblig2/vis_alle_klasser.php */
+<?php  /* vis_alle_klasser */
 /*
-/*Programmet viser alle klasser i databasen
+/*  Programmet skriver ut alle registrerte klasser
 */
-include ('db.php'); /* tilkobling til database-server utført og valg av database foretatt */
+  include (="db.php=");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
 
-$sqlSetning = "SELECT * FROM klasse;";
-$sqlResultat = mysqli_query($db, $sqlsetning) or die ("Ikke mulig å hente data fra databasen");
-/* SQL-setning sendt til database-serveren */
-$antallRader = mysqli_num_rows($sqlResultat); /* antall rader i resultatet beregnet */
+  $sqlSetning="SELECT * FROM klasse;";
+  
+  $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+    /* SQL-setning sendt til database-serveren */
+	
+  $antallRader=mysqli_num_rows($sqlResultat);  /* antall rader i resultatet beregnet */
 
-print ("<h2>Alle klasser</h2>");
-print ("<table border='1'>");
-print ("<tr> <th> Klassekode </th> <th> Klassenavn </th> <th> Studiumkode </th> </tr>");
+  print ("<h3>Registrerte klasser </h3>");
+  print ("<table border=1>");  
+  print ("<tr><th align=left>klassekode</th> <th align=left>klassenavn</th></tr>");
 
-for ($r = 1; $r <= $antallRader; $r++) {
-    $rad = mysqli_fetch_array($sqlResultat); /* ny rad hentet fra spørringsresultatet */
-    $klassekode = $rad["klassekode"]; /* eller klassekode = $rad[0]; */
-    $klassenavn = $rad["klassenavn"]; /* eller klassenavn = $rad[1]; */
-    $studiumkode = $rad["studiumkode"]; /* eller studiumkode = $rad[2]; */
-    print ("<tr> <td> $klassekode </td> <td> $klassenavn </td> <td> $studiumkode </td> </tr>");
-}
-print ("</table>");
+  for ($r=1;$r<=$antallRader;$r++)
+    {
+      $rad=mysqli_fetch_array($sqlResultat);  /* ny rad hentet fra sp�rringsresultatet */
+      $klassekode=$rad["klassekode"];        /* ELLER $klassekode=$rad[0]; */
+      $klassenavn=$rad["klassenavn"];    /* ELLER $klassenavn=$rad[1]; */
+
+      print ("<tr> <td> $klassekode </td> <td> $klassenavn </td> </tr>");
+    }
+  print ("</table>"); 
 ?>
