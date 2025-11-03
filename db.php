fafc64f1b13b837e4910cfc95a4908e5ec918c6f
RajanMohammad <rajanmohammad@hotmail.com>
2025-11-03 13:48:53 +0100

    Refactor database connection error handling
    Updated error message for database connection failure.

--- db.php
+++ db.php
@@ -1,13 +1,7 @@
-<?php 
-
-/* Programmet kobler til database-server og velger database */
-
+<?php
 $host = getenv('ramoh0645');
 $username = getenv('ramoh0645');
 $password = getenv('abf8ramoh0645');
 $database = getenv('ramoh0645');
-
-$db = mysqli_connect($host, $username, $password, $database) or die ("Kan ikke koble til database-serveren");
-/* tilkobling til database-serveren utført */
+$db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
 ?>
-
