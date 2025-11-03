d5dfed9fff4bc79b3cddc215cd1f2c3fac814939
RajanMohammad <rajanmohammad@hotmail.com>
2025-11-03 13:43:15 +0100

    Fix formatting issues in db.php

--- db.php
+++ db.php
@@ -2,11 +2,12 @@
 
 /* Programmet kobler til database-server og velger database */
 
-$host = getenv ('ramoh0645');
-$username = getenv ('ramoh0645');
-$password = getenv ('abf8ramoh0645');
-$database = getenv ('ramoh0645');
+$host = getenv('ramoh0645');
+$username = getenv('ramoh0645');
+$password = getenv('abf8ramoh0645');
+$database = getenv('ramoh0645');
 
 $db = mysqli_connect($host, $username, $password, $database) or die ("Kan ikke koble til database-serveren");
 /* tilkobling til database-serveren utført */
 ?>
+
