6e3f51290bb738a81590af3e283a97aec543b6d1
RajanMohammad <rajanmohammad@hotmail.com>
2025-11-03 03:33:36 +0100

    Fix PHP syntax and formatting issues

--- vis_alle_klasser.php
+++ vis_alle_klasser.php
@@ -2,7 +2,7 @@
 /*
 /*  Programmet skriver ut alle registrerte klasser
 */
-  include (="db.php=");  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
+  include (='db.php');  /* tilkobling til database-serveren utf�rt og valg av database foretatt */
 
   $sqlSetning="SELECT * FROM klasse;";
   
@@ -25,3 +25,4 @@
     }
   print ("</table>"); 
 ?>
+
